142d3a9ae4302641cf2506e5ffd0054c40cfdf52
Salwa99 <s.ritage99@gmail.com>
2024-04-25 16:27:28 +0100

    Implement Sign up functionalities

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  // Setup the server proxy
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3001',
+        secure: false,
+      },
+    },
+  },
   plugins: [react()],
 })
