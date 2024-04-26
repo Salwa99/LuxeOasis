4aede242b73536019d44d6c3466486637fcdf28e
Salwa99 <s.ritage99@gmail.com>
2024-04-26 19:20:39 +0100

    Setup redux store and userSlice

---
+++ client/src/redux/user/userSlice.js
@@ -0,0 +1,31 @@
+import {createSlice, current} from '@reduxjs/toolkit';
+import { sign } from 'jsonwebtoken';
+
+const initialState = {
+    currentUser: null,
+    error: null,
+    loading: false,
+};
+
+const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        signInStart: (state) => {
+            state.loading = true;
+        },
+        signInSuccess: (state, action) => {
+            state.currentUser = action.payload;
+            state.loading = false;
+            state.error = null;
+        },
+        signInFailure: (state, action) => {
+            state.error = action.payload;
+            state.loading = false;
+        }
+    }
+});
+
+export const { signInStart, signInSuccess, signInFailure } = userSlice.actions;
+
+export default userSlice.reducer;
