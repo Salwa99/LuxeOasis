905cf1a08dae42098f4202de2cc69cc602b5697b
Salwa99 <s.ritage99@gmail.com>
2024-04-26 15:37:11 +0100

    Create signin api route using jwt and cookies

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import {errorHandler} from '../utils/error.js'
+import jwt from "jsonwebtoken";
 
 export const signup = async(req, res, next) => {
     const {username, email, password} = req.body;
@@ -10,7 +12,37 @@ export const signup = async(req, res, next) => {
          await newUser.save();
          res.status(201).json("User created successfully!");
     } catch (error) {
-        next(error)
+        next(error);
     }
-   
 };
+
+export const signin = async(req, res, next) => {
+    const {email, password} = req.body;
+
+    try {
+      // Check if user exists
+      const validUser = await User.findOne({ email });
+      if (!validUser) return next(errorHandler(404, "User not found"));
+      // Validate password
+      const validPassword = bcryptjs.compareSync(password, validUser.password);
+      if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+      // Generate JWT token
+      const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      // Eliminate the password from the response
+      const {password: pass, ...restInfo} = validUser._doc;
+      // Set token as a cookie (optional, can be sent in the response body instead)
+      res
+        .cookie("access_token", token, {
+          httpOnly: true,
+          maxAge: 3600000,
+          secure: true,
+          sameSite: "strict",
+        })
+        .status(200)
+        .json(restInfo); // Send user details along with the token
+    } catch (error) {
+        next(error);
+    }
+}
--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 mongoose
   .connect("mongodb://localhost/luxeOasis")
