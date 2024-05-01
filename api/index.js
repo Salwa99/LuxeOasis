a0e3d52f38cafa45f4f42830328466edab6fa068
Salwa99 <s.ritage99@gmail.com>
2024-05-01 12:27:08 +0100

    Update the user api route for updating profile

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import dotenv from 'dotenv';
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 const PORT = 3001;
 
